ac996f764415b01b43b688878df9049e9deeb3bb
Agung12p <muchtaragung3@gmail.com>
2022-10-17 13:49:51 +0700

    change password

--- application/views/web/emt/us.php
+++ application/views/web/emt/us.php
@@ -52,7 +52,7 @@
             <div class="row">
                 <?php
                 foreach ($type as $data) { ?>
-                    <div class="col-md-4 mb-3">
+                    <div class="col-md-6 mb-3">
                         <div class="card h-100">
                             <div class="card-body">
                                 <h2 class="display-6"><?= $data->header ?></h2>
